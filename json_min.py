9ce70bd4f178a5880962786fcff30d7835a55f61
Rouge <sn.falconeye@yahoo.com>
2017-05-31 14:26:58 -0700

    Add JSON minifier
    Removes duplicate distances

---
+++ json_min.py
@@ -0,0 +1,33 @@
+import json
+
+with open('best.json', 'r') as f:
+    json_data = json.load(f)
+    print('JSON loaded')
+
+pop_queue = []
+
+l = len(json_data)
+
+i = 0
+c = 1
+while c >= 1 and i + c < len(json_data) - 1:
+    ind = i + c
+    d1 = json_data[str(i)]['dist']
+    d2 = json_data[str(ind)]['dist']
+    if d1 == d2:
+        pop_queue.append(i + c)
+        c += 1
+    else:
+        print("{} is different than {} by {} meters".format(
+                ind, i, round(d2-d1)))
+        print("Gen diff: {}".format(ind-i))
+        i = ind
+        c = 1
+
+for pop in pop_queue:
+    json_data.pop(str(pop))
+
+with open('best.min.json', 'w', newline='') as f:
+    json.dump(json_data, f, separators=(',', ': '), indent=4)
+
+print('Cut down JSON by {} items'.format(l-len(json_data)))
